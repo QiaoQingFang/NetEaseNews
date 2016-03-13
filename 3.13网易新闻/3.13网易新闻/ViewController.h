dce5881f1f5a61a4f4f23222b1c2f705b6457d9c
乔庆芳 <fangfang@qiaoqingfangdemacbook-pro.local>
2016-03-13 20:06:03 +0800

    初始化项目

---
+++ 3.13网易新闻/3.13网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3.13网易新闻
+//
+//  Created by 乔庆芳 on 16/3/13.
+//  Copyright © 2016年 乔庆芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 3.13网易新闻/3.13网易新闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  3.13网易新闻
+//
+//  Created by 乔庆芳 on 16/3/13.
+//  Copyright © 2016年 乔庆芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
